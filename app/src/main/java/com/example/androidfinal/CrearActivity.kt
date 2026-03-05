ffb925a1fc069496355107194cb44b79f8acf2ee
juniorgarciaglz2004-svg <juniorgarciaglz2004@gmail.com>
2026-03-05 19:25:00 +0100

    Actividad adicionar Completado

--- app/src/main/java/com/example/androidfinal/CrearActivity.kt
+++ app/src/main/java/com/example/androidfinal/CrearActivity.kt
@@ -1,10 +1,14 @@
 package com.example.androidfinal
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.androidfinal.bd.DbRepositorioCoches
+import com.example.androidfinal.modelos.Coche
 
 class CrearActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,22 @@ class CrearActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+        val bd = DbRepositorioCoches(this)
+        val modelo = findViewById<TextView>(R.id.addModelo)
+        val marca = findViewById<TextView>(R.id.addMarca)
+        val anno = findViewById<TextView>(R.id.addAnno)
+        val caracteristicas  = findViewById<TextView>(R.id.addCaracteristicas)
+
+        findViewById<Button>(R.id.btnCancelar).setOnClickListener {
+            finish()
+        }
+
+        findViewById<Button>(R.id.btnGuardar).setOnClickListener {
+            bd.adiciona(Coche(modelo.text.toString(),marca.text.toString(),anno.text.toString().toInt(),caracteristicas.text.toString()))
+            finish()
+        }
+
     }
 }
